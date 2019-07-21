3f8aa445c5be43253e406e62022e093c0487df5e
francis <zfc9d3f@zoho.com>
2019-07-21 13:27:19 -0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react'
+import { select, selectAll } from 'd3-selection'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const svgSelection = useRef<null | SVGSVGElement>(null)
+
+    useEffect(() => {
+        //a ref object
+        console.log(svgSelection)
+        //the instance of the dom node
+        console.log(svgSelection.current)
+        //selects the first matching element
+        console.log(select(svgSelection.current))
+        //similar to document.querySelector
+        //returns a wrapper with properties and methods that we can use to modify the dom element
+        console.log(select('.foo'), select('#bar'))
+        console.log(document.querySelectorAll('.foo'))
+        console.log(selectAll('.foo'))
+        selectAll('.foo')
+            .append('rect')
+            .attr('width', 400)
+            .attr('height', 300)
+            .attr('fill', (what, i) => {
+                console.log(what)
+                if (i === 0) {
+                    return 'blue'
+                } else {
+                    return 'red'
+                }
+            })
+    }, [svgSelection])
+    return (
+        <div>
+            <svg ref={svgSelection} />
+            <svg className="foo" />
+            <svg className="foo" />
+        </div>
+    )
 }
 
-export default App;
+export default App
