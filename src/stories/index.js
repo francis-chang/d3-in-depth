0d187bc799f27555a57d6eb62e67aa46e2bd8ce4
francis <zfc9d3f@zoho.com>
2019-07-22 08:42:22 -0700

    groups

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,9 +4,11 @@ import Selection from './components/Selection'
 import DataJoin from './components/DataJoin'
 import Enter from './components/Enter'
 import Scales from './components/Scales'
+import Group from './components/Groups'
 
 storiesOf('D3, React tutorial', module)
     .add('1 - selections', () => <Selection />)
     .add('2 - data joins', () => <DataJoin />)
     .add('2.5 - enter selection', () => <Enter />)
     .add('3 - scales', () => <Scales />)
+    .add('4 - groups and margins', () => <Group />)
