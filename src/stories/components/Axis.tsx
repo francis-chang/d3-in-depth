954e98cb47098813a4b0f0091321f77955c7043d
francis <zfc9d3f@zoho.com>
2019-07-22 12:00:12 -0700

    tick formatting

--- src/stories/components/Axis.tsx
+++ src/stories/components/Axis.tsx
@@ -37,6 +37,8 @@ const Axis: React.FC = () => {
         height: 600,
         marginLeft: 100,
         marginBottom: 100,
+        chartHeight: 500,
+        chartWidth: 500,
     }
     const svgRef = useRef<null | SVGSVGElement>(null)
     const [selection, setSelection] = useState<null | Selection<
@@ -48,7 +50,7 @@ const Axis: React.FC = () => {
 
     const y = scaleLinear()
         .domain([0, max(data, d => d.units)!])
-        .range([0, dimensions.width - dimensions.marginBottom])
+        .range([dimensions.width - dimensions.marginBottom, 0])
 
     const x = scaleBand()
         .domain(data.map(d => d.name))
@@ -59,9 +61,12 @@ const Axis: React.FC = () => {
      * generates axis functions for the given scales
      * when called, axis are rendered at the origin
      * appy transforms to place the axis where they need to be
+     * modifications can be aplied to the functions to style the axis
      */
     const xAxis = axisBottom(x)
     const yAxis = axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => `${d} units`)
 
     useEffect(() => {
         if (!selection) {
@@ -77,10 +82,20 @@ const Axis: React.FC = () => {
                 .append('g')
                 .attr(
                     'transform',
-                    `translate(${dimensions.marginLeft}, ${dimensions.height -
-                        dimensions.marginBottom})`
+                    `translate(${dimensions.marginLeft}, ${
+                        dimensions.chartHeight
+                    })`
                 )
                 .call(xAxis)
+            /**
+             * you can grab the selection of texts
+             * in the xAxisGroup to style them
+             */
+            xAxisGroup
+                .selectAll('text')
+                .attr('transform', 'rotate(-40)')
+                .attr('text-anchor', 'end')
+                .attr('font-size', '15px')
 
             const yAxisGroup = selection
                 .append('g')
@@ -95,14 +110,10 @@ const Axis: React.FC = () => {
                 .enter()
                 .append('rect')
                 .attr('width', x.bandwidth)
-                .attr('height', d => y(d.units))
+                .attr('height', d => dimensions.chartHeight - y(d.units))
                 .attr('x', d => x(d.name)!)
                 //translate the bars
-                .attr(
-                    'y',
-                    d =>
-                        dimensions.height - dimensions.marginBottom - y(d.units)
-                )
+                .attr('y', d => y(d.units))
         }
     }, [selection])
     return (
