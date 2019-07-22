7473d5fc2b530505a91ce631b389e2f2aebd48a4
francis <zfc9d3f@zoho.com>
2019-07-21 19:27:26 -0700

    scales

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react'
 import Selection from './components/Selection'
 import DataJoin from './components/DataJoin'
 import Enter from './components/Enter'
+import Scales from './components/Scales'
 
 storiesOf('D3, React tutorial', module)
     .add('1 - selections', () => <Selection />)
     .add('2 - data joins', () => <DataJoin />)
-    .add('3 - enter selection', () => <Enter />)
+    .add('2.5 - enter selection', () => <Enter />)
+    .add('3 - scales', () => <Scales />)
