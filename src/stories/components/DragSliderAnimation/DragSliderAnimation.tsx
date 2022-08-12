fb678856dbfdb5ffc3d5ce4f295d292a27e46017
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-08-13 00:43:40 +0800

    kinda woring

--- src/stories/components/DragSliderAnimation/DragSliderAnimation.tsx
+++ src/stories/components/DragSliderAnimation/DragSliderAnimation.tsx
@@ -12,121 +12,128 @@ const DragSlider2: React.FC = () => {
 
   const [moving, setMoving] = useState(false);
   // const [currentValue, setCurrentValue] = useState(0);
-  const [timer, setTimer] = useState(null);
+  // const [timer, setTimer] = useState(null);
 
   const [handle, setHandle] = useState(null);
   const [x, setX] = useState(null);
 
+  const margin = { right: 50, left: 50 };
+
   useEffect(() => {
     if (!svg) {
       setSvg(d3.select(svgRef.current));
-    } else {
-      // svg.append('rect').attr('width', 200).attr('height', 200).attr('fill', 'blue');
-
-      const margin = { right: 50, left: 50 };
-      const width = +svg.attr('width') - margin.left - margin.right;
-      const height = +svg.attr('height');
-
-      const targetValue = width;
-      let currentValue = 0;
-
-      let x = d3.scaleLinear().domain([0, 180]).range([0, width]).clamp(true);
-      setX(() => x);
-
-      let slider = svg
-        .append('g')
-        .attr('class', 'slider')
-        .attr('transform', 'translate(' + margin.left + ',' + height / 2 + ')');
-
-      slider
-        .append('line')
-        .attr('class', 'track')
-        .attr('x1', x.range()[0])
-        .attr('x2', x.range()[1])
-        .select(function () {
-          // ToDo: fix any
-          return this!.parentNode.appendChild(this.cloneNode(true)) as any;
-        })
-        .attr('class', 'track-inset')
-        .select(function () {
-          return this!.parentNode.appendChild(this.cloneNode(true));
-        })
-        .attr('class', 'track-overlay')
-        .call(
-          d3.drag().on('drag', function (event) {
-            const me = d3.select(this);
-            hue(x.invert(event.x));
-          })
-        );
-
-      slider
-        .insert('g', '.track-overlay')
-        .attr('class', 'ticks')
-        .attr('transform', 'translate(0,' + 18 + ')')
-        .selectAll('text')
-        .data(x.ticks(10))
-        .enter()
-        .append('text')
-        .attr('x', x)
-        .attr('text-anchor', 'middle')
-        .text(function (d) {
-          return d;
-        });
-
-      const handleInternal = slider.insert('circle', '.track-overlay').attr('class', 'handle').attr('r', 9);
-
-      setHandle(handleInternal);
-
-      var label = slider
-        .append('text')
-        .attr('class', 'label')
-        .attr('text-anchor', 'middle')
-        .text('0')
-        .attr('transform', 'translate(0,' + -25 + ')');
-
-      slider
-        .transition() // Gratuitous intro!
-        .duration(750)
-        .tween('hue', function () {
-          var i = d3.interpolate(0, 0);
-          return function (t) {
-            hue(i(t));
-          };
-        });
-
-      const hue = (h: any) => {
-        handleInternal.attr('cx', x(h));
-        label.attr('x', x(h)).text(Math.floor(h));
-        svg.style('background-color', d3.hsl(h, 0.8, 0.8) as any);
-      };
+      return;
     }
+    // svg.append('rect').attr('width', 200).attr('height', 200).attr('fill', 'blue');
+
+    const width = +svg.attr('width') - margin.left - margin.right;
+    const height = +svg.attr('height');
+
+    const targetValue = width;
+    let currentValue = 0;
+
+    let x = d3.scaleLinear().domain([0, 180]).range([0, width]).clamp(true);
+    setX(() => x);
+
+    let slider = svg
+      .append('g')
+      .attr('class', 'slider')
+      .attr('transform', 'translate(' + margin.left + ',' + height / 2 + ')');
+
+    slider
+      .append('line')
+      .attr('class', 'track')
+      .attr('x1', x.range()[0])
+      .attr('x2', x.range()[1])
+      .select(function () {
+        // ToDo: fix any
+        return this!.parentNode.appendChild(this.cloneNode(true)) as any;
+      })
+      .attr('class', 'track-inset')
+      .select(function () {
+        return this!.parentNode.appendChild(this.cloneNode(true));
+      })
+      .attr('class', 'track-overlay')
+      .call(
+        d3.drag().on('drag', function (event) {
+          const me = d3.select(this);
+          hue(x.invert(event.x));
+        })
+      );
+
+    slider
+      .insert('g', '.track-overlay')
+      .attr('class', 'ticks')
+      .attr('transform', 'translate(0,' + 18 + ')')
+      .selectAll('text')
+      .data(x.ticks(10))
+      .enter()
+      .append('text')
+      .attr('x', x)
+      .attr('text-anchor', 'middle')
+      .text(function (d) {
+        return d;
+      });
+
+    const handleInternal = slider.insert('circle', '.track-overlay').attr('class', 'handle').attr('r', 9);
+
+    setHandle(handleInternal);
+
+    var label = slider
+      .append('text')
+      .attr('class', 'label')
+      .attr('text-anchor', 'middle')
+      .text('0')
+      .attr('transform', 'translate(0,' + -25 + ')');
+
+    slider
+      .transition() // Gratuitous intro!
+      .duration(750)
+      .tween('hue', function () {
+        var i = d3.interpolate(0, 0);
+        return function (t) {
+          hue(i(t));
+        };
+      });
+
+    const hue = (h: any) => {
+      handleInternal.attr('cx', x(h));
+      label.attr('x', x(h)).text(Math.floor(h));
+      svg.style('background-color', d3.hsl(h, 0.8, 0.8) as any);
+    };
   }, [svg]);
 
   useEffect(() => {
+    if (!svg) return;
+
     const update = (h: number) => {
       // update position and text of label according to slider scale
-      handle.attr('cx', 20);
+      handle.attr('cx', x(h));
       svg.style('background-color', d3.hsl(h, 0.8, 0.8) as any);
     };
 
+    let currentValue: number = 0;
+    let targetValue = +svg.attr('width') - margin.left - margin.right;
+    let timer = null;
+
     const step = () => {
       console.log('step');
-      update(x.invert(20));
-      // currentValue = currentValue + targetValue / 151;
-      // if (currentValue > targetValue) {
-      //   setMoving(false);
-      //   currentValue = 0;
-      //   clearInterval(timer);
-      //   console.log('Slider moving: ' + moving);
-      // }
+      update(x.invert(currentValue));
+      currentValue = currentValue + targetValue / 151;
+      if (currentValue > targetValue) {
+        setMoving(false);
+        currentValue = 0;
+        clearInterval(timer);
+        console.log('Slider moving: ' + moving);
+      }
     };
 
     if (moving) {
-      setTimer(setInterval(step, 100));
+      timer = setInterval(step, 100);
       console.log('set interval');
     } else {
       clearInterval(timer);
-      setTimer(null);
+      timer = null;
     }
   }, [moving]);
 
